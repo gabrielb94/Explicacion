f19485dad7172ef589bd078463415ccb2c534fac
gabrielb94 <42752274+gabrielb94@users.noreply.github.com>
2018-10-31 15:13:14 -0300

    Agrego sout desde el editor de GitHub

--- Clases/Main.java
+++ Clases/Main.java
@@ -13,5 +13,7 @@ public class Main
 		System.out.println("Desde Segundo GIT pero cerrando la terminal");
 
         	System.out.println("Editado en IntelliJ");
+		
+		System.out.println("Editado en GitHub");
 	}
 }
